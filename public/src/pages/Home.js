538b060e00c0fdf012250e346a5ce9897b940789
Urmika Chandra <urmika7890@gmail.com>
2025-07-26 12:27:39 +0530

    Create Home.js

---
+++ public/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import ProductList from '../components/ProductList';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <ProductList />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
