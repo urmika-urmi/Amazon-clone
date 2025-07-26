0f97ce018abb4a0a4335a9d3fb828152cca09585
Urmika Chandra <urmika7890@gmail.com>
2025-07-26 12:38:32 +0530

    Create vite.config.js

---
+++ public/Package.json/vite.config.js
@@ -0,0 +1,24 @@
+{
+  "name": "amazon-clone",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "firebase": "^10.12.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-router-dom": "^6.23.0",
+    "react-icons": "^5.0.1",
+    "react-toastify": "^9.1.3"
+  },
+  "devDependencies": {
+    "autoprefixer": "^10.4.17",
+    "postcss": "^8.4.31",
+    "tailwindcss": "^3.4.1",
+    "vite": "^5.2.8"
+  }
+}
